653d44de41bf5b817f0adf66cab03e67ef8d1290
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-11 09:36:29 +0530

    Creating WeatherAPI interfaces and data classes for response

---
+++ app/src/main/java/com/satyamthakur/learning/weathernow/remote/WeatherDTO.kt
@@ -0,0 +1,9 @@
+package com.satyamthakur.learning.weathernow.remote
+
+import com.squareup.moshi.Json
+
+data class WeatherDTO(
+
+    @field:Json(name = "hourly")
+    val weatherData: WeatherDataDTO
+)
---
+++ app/src/main/java/com/satyamthakur/learning/weathernow/remote/WeatherDataDTO.kt
@@ -0,0 +1,22 @@
+package com.satyamthakur.learning.weathernow.remote
+
+import com.squareup.moshi.Json
+
+data class WeatherDataDTO(
+    val time: List<String>,
+
+    @field:Json(name = "temperature_2m")
+    val temperature: List<Double>,
+
+    @field:Json(name = "weathercode")
+    val weatherCode: List<Int>,
+
+    @field:Json(name = "pressure_msl")
+    val pressures: List<Double>,
+
+    @field:Json(name = "windspeed_10m")
+    val windSpeeds: List<Double>,
+
+    @field:Json(name = "relativehumidity_2m")
+    val humidities: List<Double>,
+)
