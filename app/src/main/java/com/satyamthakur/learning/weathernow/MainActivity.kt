fc8026467782d92def0f04bb9b4efa2bd502240e
Satyam Thakur <sat.thakur20@gmail.com>
2023-11-15 17:34:06 +0530

    Creating WeatherCard compose

--- app/src/main/java/com/satyamthakur/learning/weathernow/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/weathernow/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.satyamthakur.learning.weathernow
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,14 +19,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import com.satyamthakur.learning.weathernow.presentation.WeatherViewModel
+import com.satyamthakur.learning.weathernow.presentation.ui.WeatherCard
+import com.satyamthakur.learning.weathernow.ui.theme.DarkBlue
+import com.satyamthakur.learning.weathernow.ui.theme.DeepBlue
 import com.satyamthakur.learning.weathernow.ui.theme.WeatherNowTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
         setContent {
             WeatherNowTheme {
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(DarkBlue)
+                ) {
+                    WeatherCard(
+                        state = viewModel.state,
+                        backgroundColor = DeepBlue
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/satyamthakur/learning/weathernow/presentation/ui/WeatherDataDisplay.kt
@@ -0,0 +1,51 @@
+package com.satyamthakur.learning.weathernow.presentation.ui
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun WeatherDataDisplay(
+    value: Int,
+    unit: String,
+    icon: ImageVector,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle(),
+    iconTint: Color = Color.White
+) {
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = iconTint,
+            modifier = modifier.size(25.dp)
+        )
+
+        Spacer(modifier = modifier.width(4.dp))
+        Text(
+            text = "$value$unit",
+            style = textStyle
+        )
+
+    }
+
+}
